5589c77b678052d95013c6566dc85dc17d17c6ba
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-14 05:35:00 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (dwarves)
+ 
+ dwarves.each_with_index do |name,index|
+   puts "#{index+1} #{name}"
+ end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+ array.collect {|x| x.capitalize + "!"} 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  counter =0
+  while counter <array.size 
+   return true if array[counter].size>4
+  counter +=1
+end
+return false 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |array| 
+  cheese_types.include?(array)
+  end
 end
